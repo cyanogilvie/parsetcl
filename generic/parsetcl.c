d5d4230063db620a1b6a560820be9899ede60ffc
cyan <cyan.ogilvie@gmail.com>
2020-08-01 11:45:13 +0200

    Add skeleton Tcl_ObjType for parsetree, import TIP445 polyfil to support older versions of Tcl

--- generic/parsetcl.c
+++ generic/parsetcl.c
@@ -1,6 +1,60 @@
 #include "tclstuff.h"
 #include <tdom.h>
 
+static void free_parsetree(Tcl_Obj* obj);
+static void dup_parsetree(Tcl_Obj* src, Tcl_Obj* dest);
+static void update_string_rep_parsetree(Tcl_Obj* obj);
+
+Tcl_ObjType parsetree = {
+	"parsetree",
+	free_parsetree,
+	dup_parsetree,
+	update_string_rep_parsetree,
+	NULL,
+};
+
+
+static void free_parsetree(Tcl_Obj* obj) //{{{
+{
+	Tcl_ObjIntRep*	ir = NULL;
+
+	ir = Tcl_FetchIntRep(obj, &parsetree);
+	if (ir != NULL) {
+		// TODO: release the ref on the parsetree dom
+	}
+}
+
+//}}}
+static void dup_parsetree(Tcl_Obj* src, Tcl_Obj* dest) //{{{
+{
+	Tcl_ObjIntRep*		srcir = NULL;
+	Tcl_ObjIntRep		destir;
+
+	srcir = Tcl_FetchIntRep(src, &parsetree);
+	if (srcir == NULL)
+		Tcl_Panic("dup_internal_rep asked to duplicate for type, but that type wasn't available on the src object");
+
+	destir.twoPtrValue.ptr1 = srcir->twoPtrValue.ptr1;
+	destir.twoPtrValue.ptr2 = NULL;
+
+	// TODO: take a ref on the parsetree dom for the new copy of ptr1
+
+	Tcl_StoreIntRep(dest, &parsetree, &destir);
+}
+
+//}}}
+static void update_string_rep_parsetree(Tcl_Obj* obj) //{{{
+{
+	Tcl_ObjIntRep*				ir = Tcl_FetchIntRep(obj, &parsetree);
+
+	if (ir == NULL)
+		Tcl_Panic("dup_internal_rep asked to duplicate for type, but that type wasn't available on the src object");
+
+	// TODO: reconstitute the script fragment from the parsetree fragment $obj
+}
+
+//}}}
+
 #ifdef __cplusplus
 extern "C" {
 #endif  /* __cplusplus */
